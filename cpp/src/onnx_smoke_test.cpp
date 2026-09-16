1959b80cb63ed21d963505719af5bc6947203972
Evan ZIGUI <145324683+ziguieva@users.noreply.github.com>
2026-09-16 15:41:48 +0200

    build: configure ONNX Runtime C++

---
+++ cpp/src/onnx_smoke_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <onnxruntime_cxx_api.h>
+
+int main() {
+    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "smoke-test");
+
+    std::cout << "ONNX Runtime C++: OK" << std::endl;
+
+    return 0;
+}
